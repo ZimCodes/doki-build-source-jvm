7574c132e54fa35d497d15662d489d0ad781dee9
ZimCodes <23222943+ZimCodes@users.noreply.github.com>
2026-07-21 19:54:41 -0500

    refactor: move retrieving all doki theme definitions to icon project.

--- src/main/kotlin/io/unthrottled/doki/build/jvm/tools/CommonConstructionFunctions.kt
+++ src/main/kotlin/io/unthrottled/doki/build/jvm/tools/CommonConstructionFunctions.kt
@@ -3,71 +3,9 @@ package io.unthrottled.doki.build.jvm.tools
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import com.google.gson.ToNumberPolicy
-import io.unthrottled.doki.build.jvm.models.HasId
-import io.unthrottled.doki.build.jvm.models.MasterThemeDefinition
-import java.io.InputStreamReader
-import java.nio.charset.StandardCharsets
-import java.nio.file.Files
-import java.nio.file.Path
-import java.nio.file.Paths
-import java.util.stream.Collectors
-import java.util.stream.Stream
 
 object CommonConstructionFunctions {
   val gson: Gson = GsonBuilder()
     .setObjectToNumberStrategy(ToNumberPolicy.LONG_OR_DOUBLE)
     .setPrettyPrinting().create()
-
-  private fun <T : HasId> getAllProductDefinitions(
-    productBuildSourceDirectory: Path,
-    dokiProduct: DokiProduct,
-    clazz: Class<T>
-  ): Map<String, T> {
-    return Files.walk(productBuildSourceDirectory)
-      .filter { !Files.isDirectory(it) }
-      .filter { it.fileName.toString().endsWith("${dokiProduct.value}.definition.json") }
-      .map { Files.newInputStream(it) }
-      .map {
-        gson.fromJson(
-          InputStreamReader(it, StandardCharsets.UTF_8),
-          clazz
-        )
-      }.collect(
-        Collectors.toMap(
-          { it.id },
-          { it }
-        )
-      )
-  }
-
-  fun <T : HasId> getAllDokiThemeDefinitions(
-    dokiProduct: DokiProduct,
-    productBuildSourceDirectory: Path,
-    masterThemeDirectory: Path,
-    clazz: Class<T>
-  ): Stream<Triple<Path, MasterThemeDefinition, T>> {
-    val allProductDefinitions = getAllProductDefinitions(productBuildSourceDirectory, dokiProduct, clazz)
-    val masterThemeDefinitionPath = Paths.get(masterThemeDirectory.toString(), "definitions")
-    return Files.walk(masterThemeDefinitionPath)
-      .filter { !Files.isDirectory(it) }
-      .filter { it.fileName.toString().endsWith("master.definition.json") }
-      .map { it to Files.newInputStream(it) }
-      .map {
-        val masterThemePath = it.first.toString()
-        val masterFileDefinition = masterThemePath.substringAfter("$masterThemeDefinitionPath")
-        val productDefinitionDefinitionPath =
-          Paths.get(productBuildSourceDirectory.toString(), masterFileDefinition)
-        val masterThemeDefinition = gson.fromJson(
-          InputStreamReader(it.second, StandardCharsets.UTF_8),
-          MasterThemeDefinition::class.java
-        )
-        val productDefinition =
-          allProductDefinitions[masterThemeDefinition.id] ?: throw IllegalArgumentException(
-            """
-            Master Theme ${masterThemeDefinition.displayName} is missing the ${dokiProduct.prettyName} definition file!
-            """.trimIndent()
-          )
-        Triple(productDefinitionDefinitionPath, masterThemeDefinition, productDefinition)
-      }
-  }
 }
